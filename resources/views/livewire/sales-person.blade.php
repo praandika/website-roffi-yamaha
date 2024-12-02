65ab8a067af8bcbdd605d784a63a174a002c6c91
Andika Pranayoga <praandikayoga@gmail.com>
2024-12-02 18:01:01 +0800

    Update Animation

--- resources/views/livewire/sales-person.blade.php
+++ resources/views/livewire/sales-person.blade.php
@@ -1,8 +1,8 @@
 @foreach ($about as $o)
 <a href="https://wa.me/{{ $o->kontak }}?text=Hai%20saya%20ingin%20tanya%20motor%20Yamaha">
     <div class="sales">
-        <div class="detail">
-            <h4>
+        <div class="detail pulse vibrate-2">
+            <h4 class="tracking-in-expand">
                 Beli Motor? <br>
                 Hubungi Saya
             </h4>
